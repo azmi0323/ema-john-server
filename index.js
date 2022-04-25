9985bef1e1eb2448dca5edf4fc66c3badbd564af
AzmiHabib <azmihabib0323@gmail.com>
2022-04-25 13:46:10 +0600

    surver connected

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 5000;
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rhsdb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run(){
+    try{
+
+    }finally{
+        
+    }
+}
+run().catch(console.dir);
+
+
+
+app.get('/',(req,res)=>{
+    res.send('john is running and waiting')
+});
+
+app.listen(port,()=>{
+    console.log('John is running', port);
+})
